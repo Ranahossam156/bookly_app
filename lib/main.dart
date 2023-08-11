9926ece7508571f842713e3e508fda6cd3223c6a
Ranahossam156 <ranahossam156@gmail.com>
2023-08-11 17:08:43 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
